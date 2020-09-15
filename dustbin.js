ed613803eade9ea48753c95364ceba4a4c9685c8
PranavDuddu <69907209+PranavDuddu@users.noreply.github.com>
2020-09-15 19:30:59 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,10 @@
+function dustbin(){
+var box,box2,box3;
+box=createSprite(400,650,200,20)
+box2=createSprite(300,610,20,100)
+box3=createSprite(500,610,20,100)
+box.shapeColor=color(255,0,0)
+box2.shapeColor=color(255,0,0)
+box3.shapeColor=color(255,0,0)
+}
+
