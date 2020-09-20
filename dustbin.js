a0e01d442d9457db0cb889882cd344a8f52785fe
PranavDuddu <69907209+PranavDuddu@users.noreply.github.com>
2020-09-20 18:11:26 +0530

    Add files via upload

--- dustbin.js
+++ dustbin.js
@@ -1,10 +1,26 @@
-function dustbin(){
-var box,box2,box3;
-box=createSprite(400,650,200,20)
-box2=createSprite(300,610,20,100)
-box3=createSprite(500,610,20,100)
-box.shapeColor=color(255,0,0)
-box2.shapeColor=color(255,0,0)
-box3.shapeColor=color(255,0,0)
-}
-
+class Dustbin {
+    constructor(x, y, width, height) {
+      var options = {
+         isStatic:true
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      //strokeWeight(4);
+      //stroke("green");
+      fill(0);
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
+  
--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
-var b1;
 
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+var dustbin1, paperObject,groundObject;
+var world;
 
 function preload()
 {
@@ -11,13 +12,19 @@ function preload()
 }
 
 function setup() {
-	createCanvas(800, 700);
-    
-    
+	createCanvas(1250, 650);
+	rectMode(CENTER);
+
+
 	engine = Engine.create();
 	world = engine.world;
-
-	//Create the Bodies Here.
+	
+	paperObject=new Paper(200,450,70);
+	groundObject=new Ground(width/2,620,width,20);
+	dustbin1=new Dustbin(1200,510,20,200);
+	dustbin2=new Dustbin(1000,510,20,200);
+	dustbin3=new Dustbin(1100,600,200,20);
+	//Create a Ground
 
 
 	Engine.run(engine);
@@ -27,11 +34,21 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  background(0);
-  
-  drawSprites();
+  background(230);
  
+  
+  paperObject.display();
+  groundObject.display();
+  dustbin1.display();
+  dustbin2.display();
+  dustbin3.display();
 }
 
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
 
+	  Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145});
 
+  
+	}
+}
